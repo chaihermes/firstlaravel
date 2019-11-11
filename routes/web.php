8ec3e66d8b5dcba4fe55105bb61843ba606e77d8
unknown <Digital@DH-A7-11.dhbrasil.digitalhouse.com>
2019-11-11 13:10:07 -0200

    Usando laravel

---
+++ routes/web.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+// $route = new Route(); essa é a mesma forma de usar a rota, mas aqui se cria objetos. Na de baixo não se cria tantos objetos.
+
+// $route-> get();
+
+Route::get('/', function () { // o primeiro parâmetro é o que o usuário digita, o segundo é uma função anônima. Nesse caso retorna a view.
+    return view('welcome');
+});
+
+Route::get('/home', function(){
+    echo "Hello World!";
+});
